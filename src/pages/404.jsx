18c01a1945c8af72c7cd344ec87ddfe667781b39
AndreSmrnv <tehmir@bk.ru>
2021-06-24 16:33:34 +0300

    лодло

---
+++ src/pages/404.jsx
@@ -0,0 +1,21 @@
+//import React, { useEffect, useState } from 'react';
+import Link from 'next/link';
+import { MainLayout } from '@/components/MainLayout';
+import classes from '@/styles/error.module.scss';
+
+export default function ErrorPage() {
+  return (
+    <>
+    {/* <MainLayout title={'Error 404'}> */}
+      <h1 className={classes.error}>Error 404</h1>
+      <p>
+        Please{' '}
+        <Link href={'/'}>
+          <a>go back</a>
+        </Link>{' '}
+        to safety
+      </p>
+      {/* </MainLayout> */}
+    </>  
+  );
+}
